6c542c3e886e225fbce8b1112e407e51650bacc9
unknown <erica.gunn@gmail.com>
2015-10-04 20:53:03 -0400

    Draws randomized circles and squares to the svg canvas

--- script/script.js
+++ script/script.js
@@ -10,8 +10,7 @@ var plot = d3.select('.canvas')
 	.attr('height',height+margin.t+margin.b)
 	.append('g')
 	.attr('class','plot')
-	//.attr('transform','translate()') --> complete this line here
-
+	.attr('transform','translate('+margin.l+','+margin.t+')')
 
 //Start with 100 symbols
 var numOfSymbols = 100;
@@ -24,8 +23,51 @@ var numOfSymbols = 100;
 // --> type: circle or square
 // --> color
 var symbols = [];
+var x, y,sizew, sizeh,colorr,colorg, colorb,type;
+var objects = [];
 
-//With the array you've created and populated, draw circles to represent these symbols
-symbols.forEach(function(symbol){
-	
+//for each cycle up to the total number of symbols,
+for (i=0; i<numOfSymbols; i++){
+
+	//construct an object with the randomly-generated properties x, y, sizew, sizeh, color, and type
+	var objects = {
+		x:Math.random()*width,
+		y:Math.random()*height,
+		sizew:Math.random()*100,
+		sizeh: Math.random()*100,
+		//save color as a concatenation of strings and function results to give (#,#,#). Use Math.floor function to round down
+		color:'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')',
+		//assign a type to the object; 0 will be circle, 1 will be rectangle
+		type:Math.round(Math.random())
+	}
+
+	//append the new object onto the symbols array
+	symbols.push(objects);
+}
+
+//With the array we've created and populated, draw circles to represent these symbols
+
+//for each object in the symbols array, run the following function, and keep track of the array index value we're using
+symbols.forEach(function(symbol, index){
+
+	//check the value of the type attribute for an object in the array.
+	//if the type value is 0, plot a circle using the object attributes stored in the array
+	//(note that the circle does not need two size attribute values, so we'll arbitrarily use sizew to determine the radius)
+	if (symbols[index].type == 0) {
+		plot.append('circle')
+			.attr('cx', symbols[index].x)
+			.attr('cy', symbols[index].y)
+			.attr('r',symbols[index].sizew)
+			.style('fill',symbols[index].color)
+	}
+
+	//if the type values is 1, plot a rectangle instead.
+	else if (symbols[index].type == 1) {
+		plot.append('rect')
+			.attr('x', symbols[index].x)
+			.attr('y', symbols[index].y)
+			.attr('width',symbols[index].sizew)
+			.attr('height',symbols[index].sizeh)
+			.style('fill',symbols[index].color)
+	}
 })
